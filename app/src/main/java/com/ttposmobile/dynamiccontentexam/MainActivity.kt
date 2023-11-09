691c8dcc38d4cc0bdd6580f1d6b0aa4c98384c5d
CEM TASKIN <cemtaskin82@gmail.com>
2023-11-09 11:34:08 +0300

    Text Field updated

--- app/src/main/java/com/ttposmobile/dynamiccontentexam/MainActivity.kt
+++ app/src/main/java/com/ttposmobile/dynamiccontentexam/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,11 +43,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen (){
     var namesState =  remember{ mutableStateListOf<String>("Ali Duru","Aliye Duru","DENEME")}
+    var nameStateContent = remember {
+        mutableStateOf("")
+    }
 
     Column() {
-    GreetingList(namesState,{
-        namesState.add("Yeni Kişi")
-    })
+    GreetingList(namesState,
+        {
+            namesState.add(nameStateContent.value)
+            nameStateContent.value=""
+         },
+        nameStateContent.value,
+        {
+            newName -> nameStateContent.value = newName
+        }
+    )
         NamesCount(nameList = namesState)
     }
 }
@@ -53,13 +66,18 @@ fun MainScreen (){
 fun NamesCount (nameList: List<String>){
     Text(nameList.size.toString())
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingList (nameList : List<String>,buttonClick : ()->Unit){
+fun GreetingList (nameList : List<String>,
+                  buttonClick : ()->Unit,
+                  textFieldValue : String,
+                  textFieldUpdate: (newName : String) ->Unit ){
 
     Column() {
         for(name in nameList){
             Greeting(name = name)
         }
+        TextField(value = textFieldValue , onValueChange = textFieldUpdate)
 
         Button(onClick = buttonClick) {
             Text("Yeni Kişi")
